4c2b273a9e3879d5d5d4cb0edb25a9b3fb397a28
Tianrui Zhu <weltandpuzzle@gmail.com>
2025-04-18 21:52:11 -0400

    fix issues

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ app.use("/skills", require("./components/Skill/route"));
 
 app.use(cors({
   origin: ['http://localhost:3000', 
-    'https://nodejs-express-portfolio.onrender.com'],
+    'https://nodejs-express-portfolio.onrender.com',
+  'process.env.FRONTEND_URL ||'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
